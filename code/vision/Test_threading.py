2ad0e3b34a1d8d078d01bdcf7f353c6b272c0ff9
ParamDeshpande <deshpandeparam10@gmail.com>
2020-05-06 21:19:45 +0530

    test code multithreading.

--- code/vision/Test_threading.py
+++ code/vision/Test_threading.py
@@ -7,7 +7,7 @@ import time
 
 def producer(queue, event):
     """Pretend we're getting a number from the network."""
-    while (event.is_set() == False):
+    while not event.is_set():
         message = random.randint(1, 101)
         logging.info("Producer got message: %s", message)
         queue.put(message)
@@ -16,7 +16,7 @@ def producer(queue, event):
 
 def consumer(queue, event):
     """Pretend we're saving a number in the database."""
-    while (event.is_set() == False) or not queue.empty():
+    while not event.is_set() or not queue.empty():
         message = queue.get()
         logging.info(
             "Consumer storing message: %s (size=%d)", message, queue.qsize()
@@ -24,32 +24,17 @@ def consumer(queue, event):
 
     logging.info("Consumer received event. Exiting")
 
-    
-
-def f1():
-    while(event.is_set()):
-        print("event set")
-
-def f2():
-    while (not event.is_set()):
-        print("event not set")
-    
 if __name__ == "__main__":
     format = "%(asctime)s: %(message)s"
     logging.basicConfig(format=format, level=logging.INFO,
                         datefmt="%H:%M:%S")
 
-    event = threading.Event()
-    #event.set()
     pipeline = queue.Queue(maxsize=10)
-    while (1):
-        with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
-            executor.submit(f1)
-            executor.submit(f2)
-            event.set()
-            #event.clear()
-            time.sleep(0.001)
-            logging.info("Main: about to set event")
-            #event.set()
+    event = threading.Event()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+        executor.submit(producer, pipeline, event)
+        executor.submit(consumer, pipeline, event)
 
-            event.clear()
+        time.sleep(0.1)
+        logging.info("Main: about to set event")
+        event.set()
