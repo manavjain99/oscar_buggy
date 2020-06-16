99ddfa66f6aa05dc25ffb2ee2985f1d3582092c5
ParamDeshpande <deshpandeparam10@gmail.com>
2020-06-16 15:51:30 +0530

    compiling spline code w/o MCU.

--- code/vision/main.py
+++ code/vision/main.py
@@ -31,7 +31,9 @@ if __name__ == '__main__':
   import greenBallTracker as GBT 
   if INCLUDE_STM == True:
     import ComArduino2 as stcom
-  #import 
+  import numpy as np
+  from scipy.interpolate import CubicSpline
+
 
 """ WRITE YOUR VARIABLES HERE """
 
@@ -81,7 +83,7 @@ def trajectoryGen(centerXY, newXY, numpts = NO_OF_PTS):
   delPitch = -(newXY[1] - centerXY[1])/(PIX_PER_DEG+PIX_PER_DEG_VAR)
   
   # if less than min of (th% of max <s change or default).
-# if less than min of (th% of max <s change or default).
+  # if less than min of (th% of max <s change or default).
   if(abs(delYaw) < min(CHANGE_YAW_THOLD,THRES_PERCENT_CHANGE*MAX_DEL_YAW)):
     delYaw = 0
 
@@ -104,13 +106,50 @@ def trajectoryGen(centerXY, newXY, numpts = NO_OF_PTS):
 
   
 
-#def ...:
-#  """
-#  () -> ()
-#  Description: 
-#  >>>
-#  
-#  """
+def spline6pt(y):
+  """
+  (np.array[] (size = 6) ) -> (list[4], list[4], list[4])
+  Description: Generates peicewise spline curves for the 6 y pts, with x pts equally spaced as x[0:5].
+  Outputs the coeffs last 3 piecewise curves ie coeff2,coeff3,coeff4. ie ( coeffx = ax, bx, cx, dx)
+
+  regenrate the spline for coeff2 for x betn 2,3 as 
+  y = a2 + b2*(x-2) + c2*(x-2)**2 + d2*(x-2)**3
+  
+  Similarly for coeff 3 for x betn 3,4 as 
+  y = a3 + b3*(x-3) + c3*(x-3)**2 + d3*(x-3)**3
+  >>>
+  """
+  # if a valid entry 
+  if( y.size == 6):
+    x = np.array([0, 1, 2, 3, 4, 5])
+    cs = CubicSpline(x,y,bc_type='natural')
+
+    # Polynomial coefficients for 2 < x <= 3
+    a2 = cs.c.item(3,2)
+    b2 = cs.c.item(2,2)
+    c2 = cs.c.item(1,2)
+    d2 = cs.c.item(0,2)
+
+    coeff2 = [ a2, b2, c2, d2 ]
+
+    # Polynomial coefficients for 3 < x <= 4
+    a3 = cs.c.item(3,3)
+    b3 = cs.c.item(2,3)
+    c3 = cs.c.item(1,3)
+    d3 = cs.c.item(0,3)
+
+    coeff3 = [ a3, b3, c3, d3 ]
+    
+    # Polynomial coefficients for 4 < x <= 5
+    a4 = cs.c.item(3,4)
+    b4 = cs.c.item(2,4)
+    c4 = cs.c.item(1,4)
+    d4 = cs.c.item(0,4)
+
+    coeff4 = [a4, b4 , c4, d4 ]
+
+    return coeff2,coeff3,coeff4
+
 
 def grabber_thread(event, source = VID_SRC, imgQ = imageQ):
     """
@@ -163,6 +202,10 @@ def process_thread(event, source = VID_SRC, trajQ = commQ, imgQ = imageQ):
   old_objA = 0
   old_objCX = 0
   old_objCY = 0
+  frame_cx_buffer = np.array([0,0,0,0,0,0])
+  frame_cy_buffer = np.array([0,0,0,0,0,0])
+  
+  counter_comms_update = 1
   processLock = threading.Lock()
   trajList = []
   while(1):
@@ -179,9 +222,22 @@ def process_thread(event, source = VID_SRC, trajQ = commQ, imgQ = imageQ):
       objA, objCX, objCY = GBT.trackGreenBall(frame)
       logging.info(str(objA) + " " +str(objCX) + " " +str(objCY))
 
-      with processLock:
-        if INCLUDE_STM == True:
-          sendParams(objA, objCX, objCY)
+      # Shifting and Updating 1 element at a time.
+      frame_cx_buffer[0:5] = frame_cx_buffer[1:6]
+      frame_cx_buffer[5] = objCX
+      frame_cy_buffer[0:5] = frame_cy_buffer[1:6]
+      frame_cy_buffer[5] = objCY
+
+      if( counter_comms_update == 3 ):
+        with processLock:
+          if INCLUDE_STM == True:
+            cx2,cx3,cx4 = spline6pt(frame_cx_buffer)
+            cy2,cy3,cy4 = spline6pt(frame_cy_buffer)
+            sendCoeffs(cx2,cx3,cx4,cy2,cy3,cy4)
+            counter_comms_update = 1
+      else:
+        counter_comms_update = counter_comms_update + 1
+
       #logging.info("size of commsQ" + str(trajQ.qsize()))
       cv2.imshow("Process Frame", frame)
       if cv2.waitKey(1) == ord("q"):
@@ -193,6 +249,23 @@ def process_thread(event, source = VID_SRC, trajQ = commQ, imgQ = imageQ):
     
     #cv2.destroyAllWindows()
     #"""
+
+
+def sendCoeffs(cx2,cx3,cx4,cy2,cy3,cy4):
+  """
+  (list[], list[], list[], list[], list[], list[] size = 4each) -> NoneType
+  description : Sends spline coeffcients to the MCU 
+  """
+  Coeffs = ("<"\
+  +cx2[1]+','+cx2[2]+','+cx2[3]+','+cx2[4]+','\
+  +cx3[1]+','+cx3[2]+','+cx3[3]+','+cx3[4]+','\
+  +cx4[1]+','+cx4[2]+','+cx4[3]+','+cx4[4]+','\
+  +cy2[1]+','+cy2[2]+','+cy2[3]+','+cy2[4]+','\
+  +cy3[1]+','+cy3[2]+','+cy3[3]+','+cy3[4]+','\
+  +cy4[1]+','+cy4[2]+','+cy4[3]+','+cy4[4]+','\
+  +">")
+  stcom.sendToArduino(Coeffs.encode('utf-8'))
+
 
 def sendParams(objArea, objCX, objCY):
   """
