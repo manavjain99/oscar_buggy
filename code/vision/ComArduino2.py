a1032cc59600a4ae907770fd09c23df136c3aabd
ParamDeshpande <deshpandeparam10@gmail.com>
2020-06-17 19:25:25 +0530

    minor changed baudrate / mod serial port

--- code/vision/ComArduino2.py
+++ code/vision/ComArduino2.py
@@ -148,8 +148,8 @@ print
 print
 
 # NOTE the user must ensure that the serial port and baudrate are correct
-serPort = "/dev/ttyUSB0"
-baudRate = 115200
+serPort = "/dev/ttyUSB1"
+baudRate = 921600
 ser = serial.Serial(serPort, baudRate)
 print("Serial port " + serPort + " opened  Baudrate " + str(baudRate))
 
