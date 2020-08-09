5c11397153192140f6af58c0ea8e814d320d8808
ParamDeshpande <deshpandeparam10@gmail.com>
2020-08-09 22:40:47 +0530

    restored the cubic spline version , a bit of testing

--- code/playground/splinetest.py
+++ code/playground/splinetest.py
@@ -1,144 +1,92 @@
 import numpy as np
-#import scipy
-import scipy.interpolate
-import matplotlib.pyplot as plt
-
-"""
-The following code in comments was used to generate splines from Coeffs, 
-Run by splineplot.py in pilot dash the coeffs were adde to Splinecoeffs in format 
-time d c b a / or ( ab b c d ) simply compy pasted the Output for now 
-"""
+from scipy.interpolate import CubicSpline
 
 # calculate 5 natural cubic spline polynomials for 6 points
 # (x,y) = (0,12) (1,14) (2,22) (3,39) (4,58) (5,77)
 x = np.array([0, 1, 2, 3, 4, 5])
 y = np.array([12,14,22,39,58,77])
 
-# show values of interpolation function at x=1.25
+# calculate natural cubic spline polynomials
+cs = CubicSpline(x,y,bc_type='natural')
 
-#tck = scipy.interpolate.splrep(x, y)
-#pp = scipy.interpolate.interpolate.spltopp(tck[0][1:-1], tck[1], tck[2])
-#print(pp.coeffs.T)
+# show values of interpolation function at x=1.25
+print('S(1.25) = ', cs(1.25))
+
+## Aditional - find polynomial coefficients for different x regions
+
+# if you want to print polynomial coefficients in form
+# S0(0<=x<=1) = a0 + b0(x-x0) + c0(x-x0)^2 + d0(x-x0)^3
+# S1(1< x<=2) = a1 + b1(x-x1) + c1(x-x1)^2 + d1(x-x1)^3
+# ...
+# S4(4< x<=5) = a4 + b4(x-x4) + c5(x-x4)^2 + d5(x-x4)^3
+# x0 = 0; x1 = 1; x4 = 4; (start of x region interval)
+
+# show values of a0, b0, c0, d0, a1, b1, c1, d1 ...
+
+cs.c
+coeffsAll = cs.c
+print(cs.c)
+print("a0 is " + str(coeffsAll[3][0]) )
+
+# Polynomial coefficients for 0 <= x <= 1
+a0 = cs.c.item(3,0)
+print("a0 is " + str(a0) )
+
+b0 = cs.c.item(2,0)
+c0 = cs.c.item(1,0)
+d0 = cs.c.item(0,0)
+
+# Polynomial coefficients for 1 < x <= 2
+a1 = cs.c.item(3,1)
+b1 = cs.c.item(2,1)
+c1 = cs.c.item(1,1)
+d1 = cs.c.item(0,1)
+
+# Polynomial coefficients for 2 < x <= 3
+a2 = cs.c.item(3,2)
+b2 = cs.c.item(2,2)
+c2 = cs.c.item(1,2)
+d2 = cs.c.item(0,2)
+
+# Polynomial coefficients for 3 < x <= 4
+a3 = cs.c.item(3,3)
+b3 = cs.c.item(2,3)
+c3 = cs.c.item(1,3)
+d3 = cs.c.item(0,3)
+
+# Polynomial coefficients for 4 < x <= 5
+a4 = cs.c.item(3,4)
+b4 = cs.c.item(2,4)
+c4 = cs.c.item(1,4)
+d4 = cs.c.item(0,4)
+
+
+
+# Print polynomial equations for different x regions
+print('S0(0<=x<=1) = ', a0, ' + ', b0, '(x-0) + ', c0, '(x-0)^2  + ', d0, '(x-0)^3')
+print('S1(1< x<=2) = ', a1, ' + ', b1, '(x-1) + ', c1, '(x-1)^2  + ', d1, '(x-1)^3')
+
+print('...')
+#print('S5(4< x<=5) = ', a4, ' + ', b4, '(x-4) + ', c4, '(x-4)^2  + ', d4, '(x-4)^3')
+#print(str(a0)+", "+str(b0)+", "+str(c0)+", "+str(d0)+", ")
+#print(str(a1)+", "+str(b1)+", "+str(c1)+", "+str(d1)+", ")
+#print(str(a2)+", "+str(b2)+", "+str(c2)+", "+str(d2)+", ")
+#print(str(a3)+", "+str(b3)+", "+str(c3)+", "+str(d3)+", ")
+#print(str(a4)+", "+str(b4)+", "+str(c4)+", "+str(d4)+", ")
+
+# So we can calculate S(1.25) by using equation S1(1< x<=2)
+#print('S(1.25) = ', a1 + b1*0.25 + c1*(0.25**2) + d1*(0.25**3))
+
+x = np.array([0, 1, 2, 3, 4, 5])
+y[0:3] = y[3:6]
+y[3:6] = [56,745,346]   # New values. 
+
+
+# calculate natural cubic spline polynomials from 0 to 5 
+cs = CubicSpline(x,y,bc_type='natural')
+
 # send c4,c3,c2s to stm and interpolate according to those vals.    
 # Split those vals in stm according to the timestamps. 
 
 # Cubic spline interpolation calculus example
 #  https://www.youtube.com/watch?v=gT7F3TWihvk
-"""
-Python 3.8.3 (default, May 17 2020, 18:15:42) 
-[GCC 10.1.0] on linux
-Type "help", "copyright", "credits" or "license" for more information.
->>> import matplotlib.pyplot as plt
->>> from scipy.interpolate import splev, splrep
->>> x = np.linspace(0, 10, 10)
-Traceback (most recent call last):
-  File "<stdin>", line 1, in <module>
-NameError: name 'np' is not defined
->>> import numpy as np
->>> x = np.linspace(0, 10, 10)
->>> y = np.sin(x)
->>> spl = splrep(x, y)
->>> x2 = np.linspace(0, 10, 200)
->>> y2 = splev(x2, spl)
->>> plt.plot(x, y, 'o', x2, y2)
-[<matplotlib.lines.Line2D object at 0x7f09e667ef10>, <matplotlib.lines.Line2D object at 0x7f09e667e2e0>]
->>> plt.show()
->>> spl
-(array([ 0.        ,  0.        ,  0.        ,  0.        ,  2.22222222,
-        3.33333333,  4.44444444,  5.55555556,  6.66666667,  7.77777778,
-       10.        , 10.        , 10.        , 10.        ]), array([-4.94881722e-18,  8.96543619e-01,  1.39407154e+00, -2.36640266e-01,
-       -1.18324030e+00, -8.16301228e-01,  4.57836125e-01,  1.48720677e+00,
-        1.64338775e-01, -5.44021111e-01,  0.00000000e+00,  0.00000000e+00,
-        0.00000000e+00,  0.00000000e+00]), 3)
->>> spl.shape
-Traceback (most recent call last):
-  File "<stdin>", line 1, in <module>
-AttributeError: 'tuple' object has no attribute 'shape'
->>> spl.shape()
-Traceback (most recent call last):
-  File "<stdin>", line 1, in <module>
-AttributeError: 'tuple' object has no attribute 'shape'
->>> spl.dim()
-Traceback (most recent call last):
-  File "<stdin>", line 1, in <module>
-AttributeError: 'tuple' object has no attribute 'dim'
->>> type(spl)
-<class 'tuple'>
->>> tck = splrep(x, y)
->>> import scipy
->>> pp = PPoly.from_spline(tck)
-Traceback (most recent call last):
-  File "<stdin>", line 1, in <module>
-NameError: name 'PPoly' is not defined
->>> from scipy.interpolate import PPoly, splrep
->>> pp = PPoly.from_spline(tck)
->>> pp.c.T
-array([[-1.82100357e-02, -3.43151441e-01,  1.21033389e+00,
-        -4.94881722e-18],
-       [-1.82100357e-02, -3.43151441e-01,  1.21033389e+00,
-        -4.94881722e-18],
-       [-1.82100357e-02, -3.43151441e-01,  1.21033389e+00,
-        -4.94881722e-18],
-       [-1.82100357e-02, -3.43151441e-01,  1.21033389e+00,
-        -4.94881722e-18],
-       [ 1.72952212e-01, -4.64551679e-01, -5.84561936e-01,
-         7.95220057e-01],
-       [ 1.26008293e-01,  1.11955696e-01, -9.76335250e-01,
-        -1.90567963e-01],
-       [-4.93704109e-02,  5.31983340e-01, -2.60847433e-01,
-        -9.64317117e-01],
-       [-1.71230879e-01,  3.67415303e-01,  7.38484392e-01,
-        -6.65101515e-01],
-       [-1.08680287e-01, -2.03354294e-01,  9.20774403e-01,
-         3.74151231e-01],
-       [ 1.00658224e-01, -5.65621916e-01,  6.63563923e-02,
-         9.97097891e-01],
-       [ 1.00658224e-01,  1.05432909e-01, -9.56285846e-01,
-        -5.44021111e-01],
-       [ 1.00658224e-01,  1.05432909e-01, -9.56285846e-01,
-        -5.44021111e-01],
-       [ 1.00658224e-01,  1.05432909e-01, -9.56285846e-01,
-        -5.44021111e-01]])
->>> type(pp.c.T)
-<class 'numpy.ndarray'>
->>> (pp.c.T).shape()
-Traceback (most recent call last):
-  File "<stdin>", line 1, in <module>
-TypeError: 'tuple' object is not callable
->>> theArr = pp.c.T
->>> theArr.shape()
-Traceback (most recent call last):
-  File "<stdin>", line 1, in <module>
-TypeError: 'tuple' object is not callable
->>> theArr.ndim   
-2
->>> theArr
-array([[-1.82100357e-02, -3.43151441e-01,  1.21033389e+00,
-        -4.94881722e-18],
-       [-1.82100357e-02, -3.43151441e-01,  1.21033389e+00,
-        -4.94881722e-18],
-       [-1.82100357e-02, -3.43151441e-01,  1.21033389e+00,
-        -4.94881722e-18],
-       [-1.82100357e-02, -3.43151441e-01,  1.21033389e+00,
-        -4.94881722e-18],
-       [ 1.72952212e-01, -4.64551679e-01, -5.84561936e-01,
-         7.95220057e-01],
-       [ 1.26008293e-01,  1.11955696e-01, -9.76335250e-01,
-        -1.90567963e-01],
-       [-4.93704109e-02,  5.31983340e-01, -2.60847433e-01,
-        -9.64317117e-01],
-       [-1.71230879e-01,  3.67415303e-01,  7.38484392e-01,
-        -6.65101515e-01],
-       [-1.08680287e-01, -2.03354294e-01,  9.20774403e-01,
-         3.74151231e-01],
-       [ 1.00658224e-01, -5.65621916e-01,  6.63563923e-02,
-         9.97097891e-01],
-       [ 1.00658224e-01,  1.05432909e-01, -9.56285846e-01,
-        -5.44021111e-01],
-       [ 1.00658224e-01,  1.05432909e-01, -9.56285846e-01,
-        -5.44021111e-01],
-       [ 1.00658224e-01,  1.05432909e-01, -9.56285846e-01,
-        -5.44021111e-01]])
->>> 
-
-"""
