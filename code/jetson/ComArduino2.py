9188888f08bb6a0dcf987bd417a8d8f8b62215ff
Param Deshpande <37805111+ParamDeshpande@users.noreply.github.com>
2020-08-11 16:52:34 +0530

    baudrate and port changed

--- code/jetson/ComArduino2.py
+++ code/jetson/ComArduino2.py
@@ -148,8 +148,9 @@ print
 print
 
 # NOTE the user must ensure that the serial port and baudrate are correct
-serPort = "/dev/ttyUSB0"
-baudRate = 921600
+#serPort = "/dev/ttyUSB0"
+serPort = "COM4"
+baudRate = 115200
 ser = serial.Serial(serPort, baudRate)
 print("Serial port " + serPort + " opened  Baudrate " + str(baudRate))
 
