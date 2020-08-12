a6126322e550f845d7a1c867049ecf23236f8963
Param Deshpande <37805111+ParamDeshpande@users.noreply.github.com>
2020-08-12 17:33:04 +0530

    compilable

---
+++ code/stm_baremetal/stm_baremetal/Core/Inc/curves.h
@@ -0,0 +1,35 @@
+/*
+ * curves.h
+ *
+ *  Created on: Aug 12, 2020
+ *      Author: deshp
+ */
+
+#ifndef INC_CURVES_H_
+#define INC_CURVES_H_
+
+#define MAXCURVES 15
+typedef char byte;
+struct curve
+{
+  /* data */
+  float_t d;
+  float_t c;
+  float_t b;
+  float_t a;
+
+};
+
+struct Curves{
+	struct curve curves[MAXCURVES];
+};
+
+
+
+void parseCoeffs(byte* , int );
+void parseData(byte* );
+
+
+
+
+#endif /* INC_CURVES_H_ */
